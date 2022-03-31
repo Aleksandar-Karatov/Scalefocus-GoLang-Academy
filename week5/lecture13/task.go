519238a9c8415b8a24f76bd584ca692c6d39a156
Aleksandar-Karatov <32852359+Aleksandar-Karatov@users.noreply.github.com>
2022-03-31 20:23:26 +0300

    Finished week5 Lecture 13 task
    Additional code is left for an overcomplicated solution

--- week5/lecture13/task.go
+++ week5/lecture13/task.go
@@ -7,16 +7,16 @@ import (
 )
 
 func main() {
-	ctx := NewBufferedContext(time.Second, 10)
+	ctx := NewBufferedContext(55*time.Second, 1)
 
 	ctx.Run(func(ctx context.Context, buffer chan string) {
-
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			case buffer <- "bar":
-				time.Sleep(time.Millisecond * 1)
+
+				time.Sleep(time.Second * 1)
 				// try different values here
 				fmt.Println("bar")
 
@@ -27,38 +27,56 @@ func main() {
 
 type BufferedContext struct {
 	context.Context /* Add other fields you might need */
-	buffSize        int
+	buffer          chan string
+	context.CancelFunc
 }
 
 func NewBufferedContext(timeout time.Duration, bufferSize int) *BufferedContext {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout) /*Implement the rest */
-	var bc BufferedContext
-	bc.buffSize = bufferSize
-	bc.Context = ctx
-	defer cancel()
-	return &bc
+	buff := make(chan string, bufferSize)
+	newBufferCTX := &BufferedContext{Context: ctx, buffer: buff, CancelFunc: cancel}
+	//defer cancel()
+	return newBufferCTX
 }
 func (bc *BufferedContext) Done() <-chan struct{} {
 	/* This function will serve in place of the oriignal context */ /* make it so that the result channel gets closed in one of the to cases;
 	      a) the emebdded context times out
 		   b) the buffer gets filled    */
-	ch := make(chan struct{})
-	timeToEnd, _ := bc.Deadline()
-	go func() {
-		if bc.buffSize == 0 {
-			ch <- <-bc.Context.Done()
-			close(ch)
-			return
-		}
-		if time.Now().Equal(timeToEnd) {
-			ch <- <-bc.Context.Done()
-			close(ch)
-			return
-		}
-	}()
-	return ch
+
+	//timeToEnd, ok := bc.Deadline()
+
+	// err := bc.Err()
+	// ch := make(chan struct{}, bc.buffSize)
+	// go func(chan struct{}) {
+	// 	for {
+	// 		timeout := ok && time.Now().After(timeToEnd)
+	// 		if timeout {
+
+	// 			ch <- <-bc.Context.Done()
+	// 			close(ch)
+	// 			return
+	// 		}
+
+	// 		if err != nil {
+	// 			ch <- <-bc.Context.Done()
+	// 			close(ch)
+	// 			return
+	// 		}
+	// 	}
+
+	// }(ch)
+
+	//return ch
+
+	if len(bc.buffer) == cap(bc.buffer) {
+		fmt.Println("Buffer is full")
+		bc.CancelFunc()
+	}
+	return bc.Context.Done()
 }
 func (bc *BufferedContext) Run(fn func(context.Context, chan string)) {
 	/* This function serves for executing the test */ /* Implement the rest */
+
+	fn(bc, bc.buffer)
 
 }
