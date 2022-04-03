02123b156dd6cf47015484f5b4ba99d8295346eb
Aleksandar-Karatov <32852359+Aleksandar-Karatov@users.noreply.github.com>
2022-04-04 01:23:18 +0300

    week5 lecture 14 task (with .exe file)

--- week5/lecture14/task.go
+++ week5/lecture14/task.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"time"
+
 	"net/http"
 )
 
 func main() {
-	var name string
-	fmt.Print("What is your name? ")
-	fmt.Scanln(&name)
-	fmt.Printf("Hello %s!\n", name)
+	var connections int64
+	flag.Int64Var(&connections, "c", 2, "maximum number of concurrent connections ")
+	flag.Parse()
+	commands := os.Args
+	Run(connections, commands)
 }
+
+func Run(connectionsNumber int64, commands []string) {
+	ch := make(chan error, connectionsNumber)
+	tempConnectionsNumber := connectionsNumber
+	go func() {
+		for i := 3; i < len(commands); i++ {
+			if tempConnectionsNumber > 0 {
+				ch <- pingURL(commands[i])
+				time.Sleep(10 * time.Millisecond)
+				tempConnectionsNumber--
+			} else {
+				fmt.Println("Buffer full. Exiting")
+				close(ch)
+				return
+			}
+
+		}
+		close(ch)
+	}()
+	for err := range ch {
+		if len(ch) == cap(ch) {
+			close(ch)
+			return
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
 func pingURL(url string) error {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
